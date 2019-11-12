c7109f47c8438c7634c76d938e5d7966db4e75de
SzekeresBoroka <szekeresboroka@gmail.com>
2019-11-12 09:49:03 +0200

    Fragments created

---
+++ app/src/main/java/com/example/wally/AddTransactionFragment.java
@@ -0,0 +1,30 @@
+package com.example.wally;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddTransactionFragment extends Fragment {
+    private Context context;
+
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_add_transaction, container, false);
+
+        return v;
+    }
+}
