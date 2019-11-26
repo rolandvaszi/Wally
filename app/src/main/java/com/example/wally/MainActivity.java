54d5b392d608e9f17830204c0c0db187473953ca
David Pall <davidpallatx@gmail.com>
2019-11-26 13:26:25 +0200

    OnBackPress function fixed.

--- app/src/main/java/com/example/wally/MainActivity.java
+++ app/src/main/java/com/example/wally/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.wally;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -58,5 +60,27 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction frag_trans = getSupportFragmentManager().beginTransaction();
         frag_trans.replace(R.id.fragment_container,new TransactionsFragment());
         frag_trans.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //log_out();
+//        finish();
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setMessage("Are you sure you want to exit?").setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        AlertDialog alert = alertDialog.create();
+        alert.show();
     }
 }
